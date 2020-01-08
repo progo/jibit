b41c2499ef32faefa691e4874c0b330f8a40b777
Mikael Puhakka <mike@fwwm.us>
2020-01-08 20:07:44 +0200

    Remove autogenerated crap from git tracking

--- resources/public/cljs-out/dev/jibit/core_test.cljs
+++
@@ -1,5 +0,0 @@
-(ns jibit.core-test
-    (:require
-     [cljs.test :refer-macros [deftest is testing]]
-     [jibit.core :refer []]))
-
--- resources/public/cljs-out/dev/jibit/core_test.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.10.520 {}
-goog.provide('jibit.core_test');
-goog.require('cljs.core');
-goog.require('cljs.test');
-goog.require('jibit.core');
-
-//# sourceMappingURL=core_test.js.map
