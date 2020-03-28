09257b504b7e67def5597f7d24bb487a0fe8929e
Mikael Puhakka <mike@fwwm.us>
2020-03-28 14:09:22 +0200

    Normalize camera/lens make/model info.
    We'll want user to fill in missing bits and pieces, and mass tag
    photos with particular gear. For this, denormed data is the way to go.

--- src/clurator/db.clj
+++ src/clurator/db.clj
@@ -5,7 +5,7 @@
             [honeysql.helpers :as s]
             clurator.settings))
 
-(def db (jdbc/get-datasource {:dbtype "sqlite" :dbname clurator.settings/database-file}))
+(declare db)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -24,19 +24,20 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn query!
+  "Make a RO query. Take a map HoneySQL understands."
+  [query-map]
+  (jdbc/execute! db (sql/format query-map)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (def photo-clj->db-key
   "Database keys map to what processed keys."
   {
-   :lens_make           :exif/LensMake
-   :lens_model          :exif/LensModel
-   :lens_min_fl         nil
-   :lens_max_fl         nil
    :focal_length        :exif/FocalLength
    :focal_length_35     :exif/FocalLength35efl
    :aperture            :exif/Aperture
    :exposure_comp       :exif/ExposureCompensation
-   :camera_make         :exif/Make
-   :camera_model        :exif/Model
    :iso                 :exif/ISO
    :shutter_speed       :exif/ExposureTime
    :light_value         :exif/LightValue
@@ -56,8 +57,7 @@
    :original_raw        :meta/original-raw
    :original_dir        :meta/original-dir
    :uuid                :meta/uuid
-   :storage_filename    :meta/storage
-   })
+   :storage_filename    :meta/storage})
 
 (defn build-sql-keys [e]
   (->>
@@ -66,52 +66,90 @@
      [sql-key (get e foo-key)])
    (into {})))
 
+;;;;; Gear is camera or lens
+
+(defn lookup-gear
+  [gear make model]
+  (query! {:select [:id]
+           :from [gear]
+           :where [:and
+                   [:= :exif_make make]
+                   [:= :exif_model model]]}))
+
+(defn create-gear
+  [gear make model]
+  (query! {:insert-into gear
+           :values [{:exif_make make
+                     :exif_model model}]}))
+
+(defn get-or-create-gear
+  "Get or create gear with these specs. Return ID as integer. If multiple found,
+  we can't do anything and return nil."
+  [gear make model]
+  (let [found (lookup-gear gear make model)
+        cnt (count found)]
+    (cond
+      (every? nil? [make model]) nil
+      (= 1 cnt) (-> found
+                    first
+                    first
+                    second)
+      (= 0 cnt) (do
+                  (create-gear gear make model)
+                  (get-or-create-gear gear make model))
+      :else nil)))
+
 (defn add-entry!
+  "Take an Emap and insert it into DB normalised."
   [e]
-  (jdbc/execute!
-   db
-   (-> (s/insert-into :photo)
-       (s/values [(build-sql-keys e)])
-       sql/format)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn query!
-  "Make a RO query. Take a map HoneySQL understands."
-  [query-map]
-  (jdbc/execute! db (sql/format query-map)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(declare schema-sql)
-
-(defn update-schema!
-  [db]
-  (let [current-version (max 0 (database-version! db))
-        latest-version (apply max (keys schema-sql))
-        versions (range (inc current-version) (inc latest-version))]
-    (doseq [v versions]
-      (println "Updating db from v" (dec v) "to v" v)
-      (doseq [stmt (schema-sql v)]
-        (println stmt)
-        (jdbc/execute! db [stmt]))
-      (set-database-version! db v))))
+  (let [camera-id (get-or-create-gear :camera (:exif/Make e) (:exif/Model e))
+        lens-id (get-or-create-gear :lens (:exif/LensMake e) (:exif/LensModel e))]
+    (jdbc/execute!
+     db
+     (-> (s/insert-into :photo)
+         (s/values [(merge (build-sql-keys e)
+                           {:lens_id lens-id
+                            :camera_id camera-id})])
+         sql/format))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; The rest of the file will be the versioned statements in var `schema-sql`.
 
 (def schema-sql
-  {1 ["create table photo
+  {1 ["create table camera
       ( id              integer primary key autoincrement
-      , lens_make       text
-      , lens_model      text
-      , lens_min_fl     real
-      , lens_max_fl     real
+      , exif_make       text
+      , exif_model      text
+      , user_label      text
+      , unique (exif_make, exif_model)
+      );"
+      "create index ix_camera_make on camera(exif_make);"
+      "create index ix_camera_model on camera(exif_model);"
+
+      "create table lens
+      ( id              integer primary key autoincrement
+      , exif_make       text
+      , exif_model      text
+      , exif_lens_info  text
+      , user_label      text
+      , min_fl          real
+      , max_fl          real
+      , min_apt         real
+      , max_apt         real
+      , unique (exif_make, exif_model)
+      );"
+      "create index ix_lens_make on lens(exif_make);"
+      "create index ix_lens_model on lens(exif_model);"
+      "create index ix_lens_linfo on lens(exif_lens_info);"
+
+      "create table photo
+      ( id              integer primary key autoincrement
+      , camera_id       integer
+      , lens_id         integer
       , focal_length    real
       , focal_length_35 real
       , aperture        real
       , exposure_comp   real
-      , camera_make     text
-      , camera_model    text
       , iso             integer
       , shutter_speed   real
       , light_value     number
@@ -132,10 +170,12 @@
       , original_dir    text
       , uuid            text
       , storage_filename text
+      , foreign key (lens_id)   references lens(id)   on delete restrict on update cascade
+      , foreign key (camera_id) references camera(id) on delete restrict on update cascade
       );"
+      "create index ix_photo_lens_id on photo(lens_id);"
+      "create index ix_photo_camera_id on photo(camera_id);"
       "create index ix_photo_rating on photo(rating);"
-      "create index ix_photo_camera on photo(camera_make);"
-      "create index ix_photo_cameramodel on photo(camera_model);"
       "create index ix_photo_taken on photo(taken_ts);"
       "create index ix_photo_process on photo(process_ts);"
       "create index ix_photo_import on photo(import_ts);"
@@ -145,8 +185,6 @@
       "create index ix_photo_fl35 on photo(focal_length_35);"
       "create index ix_photo_ec on photo(exposure_comp);"
       "create index ix_photo_aperture on photo(aperture);"
-      "create index ix_photo_lensmake on photo(lens_make);"
-      "create index ix_photo_lensmodel on photo(lens_model);"
       "create index ix_photo_shutter on photo(shutter_speed);"
       "create index ix_photo_megapixels on photo(megapixels);"
       "create index ix_photo_width on photo(width);"
@@ -159,6 +197,42 @@
       "create index ix_photo_original_file on photo(original_file);"
       "create index ix_photo_original_raw on photo(original_raw);"
       "create index ix_photo_original_dir on photo(original_dir);"
+
+      "CREATE VIEW vw_photo AS SELECT
+      c.exif_make || ' ' || c.exif_model AS Camera,
+      l.exif_make || ' ' || l.exif_model AS Lens,
+      p.focal_length_35 AS FL,
+      p.aperture,
+      p.iso,
+      p.exposure_comp,
+      p.shutter_speed,
+      p.light_value,
+      p.rating,
+      p.orig_rating,
+      p.taken_ts,
+      p.megapixels,
+      p.storage_filename
+      FROM photo p
+      JOIN camera c ON p.camera_id = c.id
+      JOIN lens l ON p.lens_id = l.id;"
       ]
    })
 
+;; Create the connection and update the file if needed.
+
+(def db (jdbc/get-datasource {:dbtype "sqlite"
+                              :dbname clurator.settings/database-file}))
+
+(defn update-schema!
+  [db schema]
+  (let [current-version (max 0 (database-version! db))
+        latest-version (apply max (keys schema))
+        versions (range (inc current-version) (inc latest-version))]
+    (doseq [v versions]
+      (println "Updating db from v" (dec v) "to v" v)
+      (doseq [stmt (schema v)]
+        (println stmt)
+        (jdbc/execute! db [stmt]))
+      (set-database-version! db v))))
+
+(update-schema! db schema-sql)
