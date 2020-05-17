73b2da6de9f040ff7e4aef0993f2211a3a73d2dc
Mikael Puhakka <mike@fwwm.us>
2020-05-17 12:22:05 +0300

    Fix ns-qualified keyword generation in aux function

--- src/clurator/utils.clj
+++ src/clurator/utils.clj
@@ -4,6 +4,6 @@
   "Map `m` that has string keys, make them keyworded. Optional `ns`
   makes them qualified."
   ([m ns]
-   (into {} (map (fn [[k v]] [(keyword ns k) v]) m)))
+   (into {} (map (fn [[k v]] [(keyword (name ns) (name k)) v]) m)))
   ([m]
    (keywordify-map m nil)))
