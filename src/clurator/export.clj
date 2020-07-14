a9f1a05fd7234a1f68056d32b61ea1231f6f41a1
Mikael Puhakka <mike@fwwm.us>
2020-07-14 17:55:01 +0300

    Don't override exports, throw letters at the name to make unique names

--- src/clurator/export.clj
+++ src/clurator/export.clj
@@ -11,6 +11,29 @@
     (time/format "yyyy-MM-dd (ccc) HH-mm-ss"
                  (time/local-date-time taken))))
 
+(defn int->ssstring
+  "Convert integer into a spreadsheet style column name."
+  [num]
+  (loop [n num, result ""]
+    (if (pos? n)
+      (let [n' (quot (dec n) 26)
+            r  (rem  (dec n) 26)]
+        (recur n'
+               (str (char (+ 97 r)) result)))
+      result)))
+
+(defn find-first-nonconflicting-name
+  "Find if `fixed.ext` is available and if not, stuff letters in between
+  until the name is available."
+  [base ext]
+  (loop [base base, stuffings (map int->ssstring (range))]
+    (if-not (fs/exists? (str base (first stuffings) ext))
+      ;; Found an available path
+      (str base (first stuffings) ext)
+
+      (recur base (rest stuffings))
+      )))
+
 (defn export-resize-photos
   "Export selected photos as JPEGs, resize to certain size limit. With
   raw files we probably want to either run dcraw or extract the
@@ -20,9 +43,9 @@
   (doseq [photo photos]
     (let [source (str clurator.settings/storage-directory "/"
                       (:storage_filename photo))
-          target (str target-dir "/"
-                      (format-output-file-name photo)
-                      ".jpeg")]
+          target (find-first-nonconflicting-name
+                  (str target-dir "/" (format-output-file-name photo))
+                  ".jpeg")]
       (debug "Converting" source "=>" target)
       (fs/exec "convert" source
                "-resize" "1600x1600>"
